70c8663cc50b84c25e37b31ea15eadc889b36343
parzival-fowl <kidusefrem2@gmail.com>
2022-06-03 00:37:19 +0300

    run script

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main-prints a string
+ *
+ * Return: returns a value of 0 if successful
+ */
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
