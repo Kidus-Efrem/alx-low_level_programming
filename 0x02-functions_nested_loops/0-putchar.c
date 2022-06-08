e5a0c25b3a3fde25f18e30b9e93d7adc436fba48
parzival-fowl <kidusefrem2@gmail.com>
2022-06-08 17:11:06 +0300

    added void

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
 #include <stdio.h>
-/*
+/**
 *main - Print _putchar
 *
 *Return: Always (0)
 */
-int main()
+int main(void)
 {
     char *ch = "_putchar";
     int i;
-    char s = '\n';
 
     for (i = 0; i < 8; i++)
          putchar(ch[i]);
-    putchar (s);
+
+    putchar ('\n');
+
     return (0);
 }
