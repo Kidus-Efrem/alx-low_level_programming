f33e06afbd97ba7c86028a0e32a3f0acaa273cce
parzival-fowl <kidusefrem2@gmail.com>
2022-06-08 12:25:59 +0300

    run script

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,10 @@
 int main(){
     char *ch="_putchar";
     int i;
+    char s ='\n';
     for(i=0;i<8;i++)
-        {putchar(ch[i]);}
+        {
+        putchar(ch[i]);
+        }putchar(s);
     return 0;
 }
