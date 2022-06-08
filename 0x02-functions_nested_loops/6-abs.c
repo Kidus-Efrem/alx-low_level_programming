1074df836c0c905b30c3d184434e34e1a802eecd
parzival-fowl <kidusefrem2@gmail.com>
2022-06-09 01:36:11 +0300

    run script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value of an integer
+ *Return: abs
+ */
+
+int _abs(int i){
+	int i;
+	if ((i > 0) || (i==0) )
+	{
+		return (i);
+	}
+	else
+	{
+		return (-i);
+	}
+	_putchar('\n');
+}
