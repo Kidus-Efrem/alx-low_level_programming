f2683c331fb044ce4d545bf4ecf6b20857ecbade
parzival-fowl <kidusefrem2@gmail.com>
2022-06-08 20:52:00 +0300

    run script

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - checks if a character is lowercase
+ * Returns - 1 if c is lowercase and 0 if not
+ */
+
+int _islower(int c)
+{
+if (c < 97){
+	return (1);
+}
+else{
+	return (0);
+}
+}
+
