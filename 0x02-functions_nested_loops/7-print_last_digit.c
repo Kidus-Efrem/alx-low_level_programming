b397146e60b44abb3ee9580a0e08d5b3e6c5ef97
parzival-fowl <kidusefrem2@gmail.com>
2022-06-09 02:05:36 +0300

    run script

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -5,12 +5,16 @@
 int l;
 int print_last_digit(int i)
 {
-	if (i < 100)
-		l=i%10;
-	else if (i < 1000)
-		l=i%100;
-	else if (i < 10000)
-		l=i%1000;
-	return (l);
+	l=i%10;
+	if (l < 0)
+	{
+		_putchar(-l);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l);
+		return (l);
+	}
 
 }
