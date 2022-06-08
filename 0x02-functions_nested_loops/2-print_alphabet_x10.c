125325ae2cbdac869dffa74664746b56ef687baa
parzival-fowl <kidusefrem2@gmail.com>
2022-06-08 18:38:24 +0300

    run script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet -prints the lower alphabet ten times
+ * Returns -0
+ */
+void print_alphabet_x10(void){
+    int i;
+    int j;
+    for (i = 0; i <= 10; i++)
+    {
+        for (j = 97; j <= 122; j++)
+        {
+            _putchar(j);
+        }
+    }
+    _putchar('/n');
+}
