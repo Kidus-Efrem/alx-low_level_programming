6b213c36f827b7f578a0b82b71b7e21121ce7274
parzival-fowl <kidusefrem2@gmail.com>
2022-06-14 19:46:54 +0300

    betty edit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,11 +4,11 @@
  * @s: string to count length of
  * Return: length of string
 */
-int _strlen(char *s)
+int _strlen (char *s)
 {
 	int  x = 0;
 
-	while(s[x] != '\0')
+	while (s[x] != '\0')
 		x++;
 	return (x);
 }
