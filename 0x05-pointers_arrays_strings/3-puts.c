a4a43851e02864755b3893e9029974c8e544956b
parzival-fowl <kidusefrem2@gmail.com>
2022-06-14 19:54:08 +0300

    run script

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -14,12 +14,13 @@
  	putchar('\n');
  }*/
 int main(void){
-	char name[]="kidus";
-	int size=sizeof(name)/sizeof(name[0]);
-	for(int x=0;name[x]!='\0';x++)
+	char name[] = "kidus";
+	int size = sizeof(name)/sizeof(name[0]);
+	int x=0;
+
+	for(x;name[x] != '\0';x++)
 		printf("%c",name[size-x-1]);
 
-
-	return 0;
+	return (0);
 
 }
